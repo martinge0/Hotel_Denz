f94e16bada733830510350fa438f126ede62e7f3
Martin Georgiev <pchelpbs@gmail.com>
2022-11-08 11:29:38 +0200

    Title in carousel

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -2,11 +2,15 @@
   <div class="heroBlock">
     <v-carousel>
       <v-carousel-item
-        v-for="(item,i) in items"
+        v-for="(item, i) in items"
         :key="i"
         :src="item.src"
         reverse-transition="fade-transition"
-        transition="fade-transition">
+        transition="fade-transition"
+      >
+        <v-row class="fill-height" align="center" justify="center">
+          <div class="text-h1">Slide {{ i + 1 }}</div>
+        </v-row>
       </v-carousel-item>
     </v-carousel>
   </div>
@@ -16,23 +20,23 @@
 export default {
   name: "Hero-component",
 
-  data () {
-      return {
-        items: [
-          {
-            src: 'https://cdn.vuetifyjs.com/images/carousel/squirrel.jpg',
-          },
-          {
-            src: 'https://cdn.vuetifyjs.com/images/carousel/sky.jpg',
-          },
-          {
-            src: 'https://cdn.vuetifyjs.com/images/carousel/bird.jpg',
-          },
-          {
-            src: 'https://cdn.vuetifyjs.com/images/carousel/planet.jpg',
-          },
-        ],
-      }
-    },
+  data() {
+    return {
+      items: [
+        {
+          src: "https://cdn.vuetifyjs.com/images/carousel/squirrel.jpg",
+        },
+        {
+          src: "https://cdn.vuetifyjs.com/images/carousel/sky.jpg",
+        },
+        {
+          src: "https://cdn.vuetifyjs.com/images/carousel/bird.jpg",
+        },
+        {
+          src: "https://cdn.vuetifyjs.com/images/carousel/planet.jpg",
+        },
+      ],
+    };
+  },
 };
 </script>
