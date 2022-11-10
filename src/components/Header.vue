9747ddfdd656edc9530bfedc3db06529a06ac29a
Martin Georgiev <pchelpbs@gmail.com>
2022-11-10 13:02:51 +0200

    Before using Drawer

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -5,11 +5,16 @@
 
       <v-spacer></v-spacer>
       <v-toolbar-items>
-        <v-btn text>Начало</v-btn>
+        <v-btn text v-show="$vuetify.breakpoint.smAndUp">Начало</v-btn>
 
-        <v-btn text>За нас</v-btn>
+        <v-btn text v-show="$vuetify.breakpoint.smAndUp">За нас</v-btn>
 
-        <v-btn text>Контакти</v-btn>
+        <v-btn text v-show="$vuetify.breakpoint.smAndUp">Контакти</v-btn>
+        <v-app-bar-nav-icon
+          v-show="!$vuetify.breakpoint.smAndUp"
+          @click.stop="drawer = !drawer"
+        >
+        </v-app-bar-nav-icon>
       </v-toolbar-items>
     </v-toolbar>
   </v-container>
@@ -19,6 +24,15 @@
 export default {
   name: "Header-component",
 
-  data: () => ({}),
+  data: () => ({
+    drawer: false,
+    group: null,
+  }),
+
+  watch: {
+    group() {
+      this.drawer = false;
+    },
+  },
 };
 </script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,15 +1,20 @@
 <template>
-  <Hero />
+  <v-content>
+    <Hero />
+    <Gallery />
+  </v-content>
 </template>
 
 <script>
 import Hero from "../components/Hero";
+import Gallery from "../components/Gallery";
 
 export default {
   name: "Home-page",
 
   components: {
     Hero,
+    Gallery,
   },
 };
 </script>
